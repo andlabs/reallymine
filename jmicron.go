299fac1e470f27f1bfe4a6b818e512b2dc23e79c
Pietro Gagliardi <pietro10@mac.com>
2015-10-22 13:51:46 -0400

    More JMicron work.

--- jmicron.go
+++ jmicron.go
@@ -22,55 +22,65 @@ func (JMicron) NeedsKEK() bool {
 	return true
 }
 
-func (JMicron) CreateDecrypter(keySector []byte, kek []byte) (cipher *aes.Cipher) {
-	var i int
-
-	// decrypt the key sector
+func (JMicron) decryptKeySector(keySector []byte, kek []byte) {
 	Reverse(kek)
-	kekcipher, err := aes.NewCipher(kek)
-	if err != nil {
-		panic("[BUG] error creating KEK decrypter in JMicron.CreateDecrypter()")
-	}
+	kekcipher := NewAES(kek)
 	for i := 0; i < len(keySector); i += 16 {
-		Reverse(keySector[i:i + 16])
-		kekcipher.Decrypt(keySector[i:i + 16], keySector[i:i + 16])
-		Reverse(keySector[i:i + 16])
+		block := keySector[i:i + 16]
+		Reverse(block)
+		kekcipher.Decrypt(block, block)
+		Reverse(block)
 	}
+}
 
-	// find the DEK itself
-	// this can be anywhere in the block
-	found := false
+// the DEK can be anywhere in the decrypted key sector
+func (JMicron) findDEK(keySector []byte) (offset int) {
 	for i = 0; i < len(keySector) - 4; i++ {
-		found = keySector[i + 0] == 'D' &&
+		if keySector[i + 0] == 'D' &&
 			keySector[i + 1] == 'E' &&
 			keySector[i + 2] == 'K' &&
-			keySector[i + 3] == '1'
-		if found {
-			break
+			keySector[i + 3] == '1' {
+			return i
 		}
 	}
-	if !found {			// wrong KEK, sorry
-		return nil
-	}
+	return -1		// not found; this isn't the right KEK
+}
 
-	// the first half of the key is the 16 bytes at i + 0xC
-	// the second half of the key is the 16 bytes at i + 0x20
-	// the key size is the byte at i + 0x58
-	if keySector[i + 0x58] != 0x20 {
-		panic("[HELP] TODO write this")
+func (JMicron) extractDEK(keySector []byte, offset int) []byte {
+	keySector = keySector[offset:]
+	// the first half of the key is the 16 bytes at offset 0xC
+	// the second half of the key is the 16 bytes at offset 0x20
+	// the key size is the byte at offset 0x58
+	if keySector[0x58] != 0x20 {
+		HELP("The size of the encryption key in your JMicron sector is not known.\nThis means your drive is new to reallymine, and support must be added.\nPlease help us!")
 	}
 	dek := make([]byte, 32)
-	copy(dek[:16], keySector[i + 0xC:])
-	copy(dek[16:], keySector[i + 0x20:])
+	copy(dek[:16], keySector[0xC:])
+	copy(dek[16:], keySector[0x20:])
 	Reverse(dek)
-	dekcipher, err := aes.NewCipher(dek)
-	if err != nil {
-		panic("[BUG] TODO")
+	return dek
+}
+
+func (j JMicron) CreateDecrypter(keySector []byte, kek []byte) (cipher *aes.Cipher) {
+	// make a copy of these so the originals aren't touched
+	keySector = DupBytes(keySector)
+	kek = DupBytes(kek)
+
+	j.decryptKeySector(keySector, kek)
+	offset := j.findDEK(keySector)
+	if offset == -1 {		// wrong KEK
+		return nil
 	}
-	return dekcipher
+	return NewAES(j.extractDEK(keySector, offset))
 }
 
 func (JMicron) Decrypt(c *aes.Cipher, b []byte) {
+	for i := 0; i < len(b); i += 16 {
+		block := b[i:i + 16]
+		Reverse(block)
+		c.Decrypt(block, block)
+		Reverse(block)
+	}
 }
 
 func init() {
--- util.go
+++ util.go
@@ -1,9 +1,16 @@
 // 21 october 2015
 package main
 
+import (
+	"crypto/aes"
+)
+
 // Byte shuffling.
 
 func Reverse(b []byte) {
+	if len(b) % 2 == 1 {
+		BUG("Reverse() called with odd-sized slice")
+	}
 	for i := 0; i < len(b) / 2; i++ {
 		n := len(b) - i - 1
 		b[i], b[n] = b[n], b[i]
@@ -11,6 +18,9 @@ func Reverse(b []byte) {
 }
 
 func SwapLongs(b []byte) {
+	if len(b) % 4 != 0 {
+		BUG("SwapLongs() called with len(b) not a multiple of 4")
+	}
 	for i := 0; i < len(b); i += 4 {
 		b[i + 0], b[i + 3] = b[i + 3], b[i + 0]
 		b[i + 1], b[i + 2] = b[i + 2], b[i + 1]
@@ -20,11 +30,27 @@ func SwapLongs(b []byte) {
 func SwapHalves(b []byte) {
 	n := len(b)
 	if n % 2 == 1 {
-		panic("[BUG] odd length slice passed to SwapHalves()")
+		BUG("SwapHalves() called with odd-sized slice")
 	}
 	n /= 2
 	c := make([]byte, len(b))
 	copy(c, b[n:])
 	copy(c[n:], b)
 	copy(b, c)
+}
+
+// Other functions.
+
+func DupBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
+
+func NewAES(key []byte) *aes.Cipher {
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		BUG("error creating AES cipher in NewAES(): %v", err)
+	}
+	return c
 }
