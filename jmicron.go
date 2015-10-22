1bd02985069b105e39aaf320bd03da8fda55302a
Pietro Gagliardi <pietro10@mac.com>
2015-10-22 14:00:34 -0400

    More JMicron work.

--- jmicron.go
+++ jmicron.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"crypto/aes"
 )
 
@@ -46,17 +48,45 @@ func (JMicron) findDEK(keySector []byte) (offset int) {
 	return -1		// not found; this isn't the right KEK
 }
 
+// The names Key3EE2, Key3EF2, and Key3F02 are from the paper.
+// But I recognize the hex numbers as addresses in the JMicron chip's
+// RAM. These RAM addresses followed me around throughout
+// disassembly, and I *knew* they were suspicious, damnit!
+type jmicromDEKBlock struct {
+	Magic		[4]byte		// 'DEK1'
+	Checksum	uint16
+	Unknown		uint16
+	Random1		uint32
+	Key3EE2		[16]byte		// This is the first half of the AES-256 key.
+	Random2		uint32
+	Key3EF2		[16]byte		// This is the second half of the AES_256 key.
+	Random3		uint32
+	Key3F02		[32]byte		// I don't know what this is but I highly doubt it's a key.
+	Random4		uint32
+	KeySize		byte
+	Remaining	[1 + 4 + 2]byte
+}
+
 func (JMicron) extractDEK(keySector []byte, offset int) []byte {
-	keySector = keySector[offset:]
-	// the first half of the key is the 16 bytes at offset 0xC
-	// the second half of the key is the 16 bytes at offset 0x20
-	// the key size is the byte at offset 0x58
-	if keySector[0x58] != 0x20 {
+	var dekblock jmicronDEKBlock
+
+	r := bytes.NewReader(keySector[offset:])
+	// The endianness is likely wrong. We don't use any of
+	// the endian-dependent fields, though. I can figure the
+	// correct endianness from the disassembly if they're ever
+	// actually needed.
+	err := binary.Read(r, binary.BigEndian, &dekblock)
+	if err != nil {
+		BUG("error reading out DEK block from decrypted key sector in JMicron.extractDEK(): %v", err)
+	}
+
+	if dekblock.KeySize != 0x20 {
 		HELP("The size of the encryption key in your JMicron sector is not known.\nThis means your drive is new to reallymine, and support must be added.\nPlease help us!")
 	}
+
 	dek := make([]byte, 32)
-	copy(dek[:16], keySector[0xC:])
-	copy(dek[16:], keySector[0x20:])
+	copy(dek[:16], dekblock.Key3EE2[:])
+	copy(dek[16:], dekblock.Key3EF2[:])
 	Reverse(dek)
 	return dek
 }
