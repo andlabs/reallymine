9e199fae42923542263d4d2c65c0317200fd5274
Pietro Gagliardi <pietro10@mac.com>
2016-09-21 22:19:12 -0400

    Preparations for the remaining commands, which require being able to read passwords.

--- common.go
+++ common.go
@@ -3,10 +3,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"encoding/hex"
 
 	"github.com/andlabs/reallymine/disk"
 	"github.com/andlabs/reallymine/bridge"
+	"github.com/andlabs/reallymine/kek"
+	"github.com/hashicorp/vault/helper/password"
 )
 
 type foundKeySector struct {
@@ -39,8 +42,54 @@ func findKeySector(d *disk.Disk, startAt int64) (fks *foundKeySector, err error)
 	return fks, nil
 }
 
-func askForPassword() (password string, err error) {
-	panic("TODO")
+func askForPassword() (pw string, err error) {
+	fmt.Print("Enter WD password: ")
+	return password.Read(os.Stdin)
+}
+
+func tryGetKeySectorAndDEK(b bridge.Bridge, sector []byte) (raw []byte, dek []byte, err error) {
+	var ks bridge.KeySector
+	var curkek []byte
+
+	try := func() {
+		ks, err = b.DecryptKeySector(sector, curkek)
+		if err == nil {
+			dek, err = ks.DEK()
+		}
+	}
+
+	if !b.NeedsKEK() {
+		dek, err = ks.DEK()
+		if err != nil {
+			return nil, nil, err
+		}
+		return ks.Raw(), dek, nil
+	}
+
+	curkek = kek.Default
+	first := true
+	try()
+	for err == bridge.ErrWrongKEK {
+		if first {
+			fmt.Printf("You need the WD password to decrypt this drive.\n")
+			first = false
+		} else {
+			fmt.Printf("Wrong WD password.\n")
+		}
+		pw, err := askForPassword()
+		if err == password.ErrInterrupted {		// cancelled
+			return nil, nil, nil
+		}
+		if err != nil {
+			return nil, nil, err
+		}
+		curkek = kek.FromPassword(pw)
+		try()
+	}
+	if err != nil {
+		return nil, nil, err
+	}
+	return ks.Raw(), dek, nil
 }
 
 func dumpSector(sector []byte, pos int64) string {
