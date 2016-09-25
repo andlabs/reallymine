76731453476533912420768d98c338fd60917aa0
Pietro Gagliardi <pietro10@mac.com>
2016-09-24 21:36:34 -0400

    Replaced what's left of common.go with just a bunch of formatting functions.

---
+++ format.go
@@ -0,0 +1,22 @@
+// 22 october 2015
+package main
+
+import (
+	"fmt"
+	"encoding/hex"
+	"strings"
+
+	"github.com/andlabs/reallymine/bridge"
+)
+
+func formatSectorPos(pos int64) string {
+	return fmt.Sprintf("sector at 0x%X", pos)
+}
+
+func formatBridge(b bridge.Bridge) string {
+	return fmt.Sprintf("bridge type %s", b.Name())
+}
+
+func formatKey(key []byte) string {
+	return strings.ToUpper(hex.EncodeToString(key))
+}
